918570608b6d3cb757d0d6707cd660b0d41e1984
Josh Lee <joshua.huggins@sbcglobal.net>
2024-04-29 16:29:31 -0500

    added all the code for routing

--- controllers/index.js
+++ controllers/index.js
@@ -4,5 +4,9 @@ const router = require('express').Router();
 
 // Import the index.js from api folder
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes')
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
