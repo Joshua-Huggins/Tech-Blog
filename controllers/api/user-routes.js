df19c20ba91bc18a933afcc9bf9f5b2d267567d4
Josh Lee <joshua.huggins@sbcglobal.net>
2024-04-30 22:06:06 -0500

    fixed issue with routing

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -28,10 +28,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create({
-            ...req.body,
-            user_id: req.session.user_id
-        });
+       const userData = await User.create(req.body);
         res.status(200).json(userData);
     } catch (err) {
         res.status(400).json(err);
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXISTS tech_blogdb;
 
 CREATE DATABASE tech_blogdb;
+
+\c tech_blogdb;
+
+CREATE TABLE user (username VARCHAR(30), email VARCHAR(30), password VARCHAR(30));
+
+INSERT INTO user (username, email, password) VALUES (Josh, joshua.huggins@sbcglobal.net, blahblahblah);
